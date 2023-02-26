5576b40844e0fed2355bc7bb0b3f0834f179288b
JuanPabloVerduzcoCampos <90339360+JuanPabloVerduzcoCampos@users.noreply.github.com>
2023-02-25 19:18:57 -0600

    Practica 8

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+var http = require ('http'); //Importamos el modulo http
 
+http.createServer(function (req, res) { //Creamos el servidor que escucha en el puerto 1337 y muestra el mensaje Hello World
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+    
+}).listen(1337, '127.0.0.1'); //Escucha en el puerto 1337 y en la ip
